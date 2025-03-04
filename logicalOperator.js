fdc17f8ab42a2389922529d18611a25603948839
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-04 15:48:08 +0600

    logical operator

---
+++ logicalOperator.js
@@ -0,0 +1,11 @@
+// And (&&), Or (||)
+
+const person = {
+  name: "Sifat",
+  gender: "male",
+  isStudent: true,
+  //   isStudent: false,
+};
+
+// console.log(person.isStudent && "He is a Student.");
+console.log(person.name || "Not Available.");
