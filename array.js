bb3f07544ff083a204d569a6f149b61765a9483b
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-04 16:00:48 +0600

    Array spread, destructure and rest array operator

---
+++ array.js
@@ -0,0 +1,25 @@
+// ARRAY Spreading
+
+// const numbers = [34, 85, 46, 86];
+
+// const numbersTwo = numbers;
+// const numbersTwo = [...numbers];
+
+// numbersTwo.push(66);
+// numbers.push(22);
+
+// console.log(numbers);
+// console.log(numbersTwo);
+
+// ARRAY Destructuring
+
+const numbers = [55, 66, 88, 15, 21, 469];
+
+// const [x, y] = numbers;
+
+// Array REST Operator
+const [x, y, ...z] = numbers;
+
+// console.log(x, y);
+// console.log(x, y, z);
+console.log(z);
