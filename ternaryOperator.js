ab322e964f6da7d6d6be72eec0860c146427c95f
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-04 15:19:08 +0600

    Ternary Operator

---
+++ ternaryOperator.js
@@ -0,0 +1,22 @@
+// Ternary Operator
+
+const person = {
+  name: "Sifat",
+  gender: "male",
+    isStudent: true,
+//   isStudent: false,
+};
+
+// let sentence = "";
+
+// if (person.isStudent) {
+//   sentence = "He is a student.";
+// } else {
+//   sentence = "He is not a student";
+// }
+
+// console.log(sentence);
+
+let sentence = person.isStudent ? "He is a Student" : "He is not a student.";
+
+console.log(sentence);
