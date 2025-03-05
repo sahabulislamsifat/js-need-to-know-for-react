1a5cfe4d6df20528801cd0696689f1733933b5e6
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-05 19:32:27 +0600

    set interval

--- scope.js
+++ scope.js
@@ -21,3 +21,57 @@
 //   let x = 10;
 //   console.log(x);
 // }
+
+// let i = 0;
+
+// while (i < 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// const array = [1, 2, 3, 4, 5, 6, 7];
+
+// for (let i = 1; i < array.length; i++) {
+//   if (i == 5) {
+//     continue;
+//   }
+//   console.log(array[i]);
+// }
+
+// let str = "Sifattttttt";
+
+// console.log(str.indexOf("i", 3));
+
+// const myObject = {
+//   a: 1,
+//   b: 2,
+//   c: 3,
+// };
+
+// for (let item in myObject) {
+//   console.log(myObject[item]);
+
+
+// function getMenu() {
+//   console.log("Burger, Pizza, Pasta");
+// }
+
+// getMenu()
+
+// for (var i = 0; i < 5; i++) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, 100);
+// }
+
+let count = 10;
+
+let countdown = setInterval(() => {
+  console.log(count);
+  count--;
+
+  if (count < 0) {
+    clearInterval(countdown); // কাউন্টডাউন ০ হলে বন্ধ হবে
+    console.log("Time's up!");
+  }
+}, 1000);
